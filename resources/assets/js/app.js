e98550d61b1117076f29bd8428978b19235be315
Vidhya Kumar <vidhya.kumar@outlook.com>
2016-05-15 20:27:00 +0800

    Added ajax function for mark as complete feature.

---
+++ resources/assets/js/app.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('.completeTask').submit(function(event){
+    event.preventDefault();
+    var formData = $(this).serializeArray();
+    var formAction = $(this).attr('action');
+    $(this).children('button').toggleClass('btn-primary btn-success');
+    $.post(formAction, formData, function(data){
+      if (data.completed) {
+        $('#task-' + data.id).addClass('text-muted strike');
+      } else {
+        $('#task-' + data.id).removeClass('text-muted strike');
+      }
+    }, 'json');
+  });
+});
