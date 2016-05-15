9b4bc0f488091f26d465f1157dd60146a2f81165
Vidhya Kumar <vidhya.kumar@outlook.com>
2016-05-15 20:25:41 +0800

    Added & refactored controller with create/update/delete methods using eloquent.

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,40 +2,54 @@
 
 namespace App\Http\Controllers;
 
-use Auth;
 use App\Task;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 
 class TasksController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-      $this->middleware('auth');
-    }
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-      $tasks = Auth::user()->tasks()->orderBy('created_at', 'desc')->get();
-      return view('tasks', compact('tasks'));
-    }
+  /**
+   * Show the application dashboard.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index(Request $request)
+  {
+    $tasks = $request->user()->tasks()
+             ->orderBy('created_at', 'desc')
+             ->get();
+    return view('tasks', compact('tasks'));
+  }
 
-    public function create(Request $request)
-    {
-      $this->validate($request, ['task' => 'required']);
-      $task = new Task($request->all());
-      $task->user_id = Auth::id();
-      $task->save();
-      return back();
-    }    
+  public function create(Request $request)
+  {
+    $this->validate($request, ['task' => 'required']);
+    $task = new Task($request->all());
+    $request->user()->addTask($task);
+    return back();
+  }
+
+  public function update(Request $request)
+  {
+    $task = Task::findOrFail($request->id);
+    $task->completed = $task->completed == 1 ? false : true;
+    $task->save();
+    return $task;
+  }
+
+  public function delete(Request $request)
+  {
+    $task = Task::findOrFail($request->id)->delete();
+    return back();
+  }
 }
