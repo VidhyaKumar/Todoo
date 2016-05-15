7809255756090d0e075a5c192ce2f1d103d44dea
Vidhya Kumar <vidhya.kumar@outlook.com>
2016-05-15 19:06:17 +0800

    Moved inline task related forms into custom partials.

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -6,27 +6,8 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Create Task</div>
-
                 <div class="panel-body">
-                    <!-- Start Create Task Form -->
-                    <form method="post" action="/tasks">
-                      {{ csrf_field() }}
-                      <div class="form-group{{ $errors->has('task') ? ' has-error' : '' }}">
-                        <label for="createTask">Task</label>
-                        <input type="text" class="form-control" id="createTask" placeholder="Example: Buy milk!" name="task">
-                        @if ($errors->has('task'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('task') }}</strong>
-                            </span>
-                        @endif
-                      </div>
-                      <div class="form-group">
-                        <button type="submit" class="btn btn-default">
-                          <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add Task
-                        </button>
-                      </div>
-                    </form>
-                    <!-- End Create Task Form -->
+                  @include('forms.task_new')
                 </div>
             </div>
         </div>
@@ -44,23 +25,15 @@
                       @if (count($tasks) > 0)
                         @foreach ($tasks as $task)
                         <tr><td>
-                            {{ $task->task }}
-                            <div class="pull-right">
-                              <form class="inline" action="/tasks/{{ $task->id }}" method="POST">
-                                {{ csrf_field() }}
-                                {{ method_field('PATCH') }}
-                                <button type="submit" class="btn btn-success btn-xs">
-                                  <span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Complete
-                                </button>
-                              </form>
-                              <form onsubmit="return confirm('Are you sure?');" class="inline" action="/tasks/{{ $task->id }}" method="POST">
-                                {{ csrf_field() }}
-                                {{ method_field('DELETE') }}
-                                <button type="submit" class="btn btn-danger btn-xs">
-                                  <span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Delete
-                                </button>
-                              </form>
-                            </div>
+                          @if ($task->completed)
+                            <span id="task-{{ $task->id }}" class="text-muted strike">{{ $task->task }}</span>
+                          @else
+                            <span id="task-{{ $task->id }}">{{ $task->task }}</span>
+                          @endif
+                          <span class="pull-right">
+                            @include('forms.task_complete')
+                            @include('forms.task_delete')                            
+                          </span>
                         </td></tr>
                         @endforeach
                       @else
