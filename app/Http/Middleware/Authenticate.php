b149d80bac9291c090302f43a27a548acd9b5be7
Vidhya Kumar <vidhya.kumar@outlook.com>
2016-05-16 20:03:22 +0800

    Modified middleware to identify API endpoint request.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -17,14 +17,18 @@ class Authenticate
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->guest()) {
-            if ($request->ajax() || $request->wantsJson()) {
-                return response('Unauthorized.', 401);
-            } else {
-                return redirect()->guest('login');
-            }
+      if (Auth::guard($guard)->guest()) {
+        if ($request->ajax() || $request->wantsJson() || $request->is('api/*')) {
+          $error = [
+            'code' => '401',
+            'message' => 'Unauthorized.'
+          ];
+          return response($error, 401);
+        } else {
+          return redirect()->guest('login');
         }
+      }
 
-        return $next($request);
+      return $next($request);
     }
 }
