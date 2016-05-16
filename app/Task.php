75d3ab6451b4beff1f73784a8efc33be43a17250
Vidhya Kumar <vidhya.kumar@outlook.com>
2016-05-16 19:58:01 +0800

    No changes.

--- app/Task.php
+++ app/Task.php
@@ -7,9 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $fillable = ['task'];
+    protected $hidden = ['user_id'];
 
     public function user()
     {
         return $this->belongsTo(User::class);
-    }    
+    }
 }
